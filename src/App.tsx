93e254180ed877b5bcfff6c03dbaec6212ec065e
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-12-25 23:17:46 +0530

    added nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [time, setTime] = useState(0);
 
   return (
     <>
-      <div>Panda Type</div>
+    <div className="flex justify-center items-center mt-2">
+      <nav className="flex justify-between items-center w-full max-w-4xl px-4 py-2 bg-[#2c2e31] shadow-md rounded-lg">
+        <h1 className="text-2xl font-bold text-[#62665e]">PandaType</h1>
+        <div className="flex space-x-4">
+          <button
+            onClick={() => setTime(15)}
+            className={`py-2 px-4 rounded-lg text-[#62665e] font-semibold ${time === 15 ? 'text-[#e2b71e]' : 'hover:text-white'}`}
+          >
+            15
+          </button>
+          <button
+            onClick={() => setTime(30)}
+            className={`py-2 px-4 rounded-lg text-[#62665e] font-semibold ${time === 30 ? 'text-[#e2b71e]' : ' hover:text-white'}`}
+          >
+            30
+          </button>
+        </div>
+      </nav>
+    </div>
+
+    {/* Typing Area */}
+    
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
