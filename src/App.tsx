99c909fbd6c1c24aa6a168c189f25f7087bd37b4
LoveKumarAgrawal <loveagrawal204@gmail.com>
2025-01-01 15:08:10 +0530

    added keypress logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,101 @@ import { words } from './constants/sentence';
 
 function App() {
   const [time, setTime] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
-  function addClass(el: Element | null, name: string) {
+  const handleKeyUp = (event: any) => {
+    const key = event.key;
+    const currentWord = document.querySelector('.word.current');
+    const currentLetter = document.querySelector('.letter.current');
+    const expected = currentLetter?.innerHTML || ' ';
+    const isLetter = key.length === 1 && key !== ' ';
+    const isSpace = key === ' ';
+    const isBackspace = key === 'Backspace';
+    const isFirstLetter = currentLetter === currentWord?.firstChild;
+
+    if (isLetter) {
+      if (currentLetter) {
+        addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
+        removeClass(currentLetter, 'current');
+        if (currentLetter.nextSibling) {
+          addClass(currentLetter.nextSibling, 'current');
+        }
+      } else {
+        const incorrectLetter = document.createElement('span');
+        incorrectLetter.innerHTML = key;
+        incorrectLetter.className = 'letter incorrect extra';
+        currentWord?.appendChild(incorrectLetter);
+      }
+    }
+
+    if (isSpace) {
+      if (expected !== ' ') {
+        const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')];
+        lettersToInvalidate.forEach((letter) => {
+          addClass(letter, 'incorrect');
+        });
+      }
+      removeClass(currentWord, 'current');
+      addClass(currentWord?.nextSibling, 'current');
+      if (currentLetter) {
+        removeClass(currentLetter, 'current');
+      }
+      addClass(currentWord?.nextSibling?.firstChild, 'current');
+    }
+
+    if (isBackspace) {
+      if (currentLetter && isFirstLetter) {
+        removeClass(currentWord, 'current');
+        if (currentWord.previousSibling) {
+          addClass(currentWord.previousSibling, 'current');
+          removeClass(currentLetter, 'current');
+          addClass(currentWord.previousSibling.lastChild, 'current');
+          removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+          removeClass(currentWord.previousSibling.lastChild, 'correct');
+        } else {
+          addClass(currentWord, 'current');
+          addClass(currentLetter, 'current');
+        }
+      } else if (currentLetter && !isFirstLetter) {
+        removeClass(currentLetter, 'current');
+        addClass(currentLetter.previousSibling, 'current');
+        removeClass(currentLetter.previousSibling, 'incorrect');
+        removeClass(currentLetter.previousSibling, 'correct');
+      } else if (!currentLetter) {
+        addClass(currentWord?.lastChild, 'current');
+        removeClass(currentWord?.lastChild, 'incorrect');
+        removeClass(currentWord?.lastChild, 'correct');
+      }
+    }
+  };
+
+  const addClass = (el: any, name: string) => {
     if (el) el.classList.add(name);
-  }
-  
-  function removeClass(el: Element | null, name: string) {
+  };
+
+  const removeClass = (el: any, name: string) => {
     if (el) el.classList.remove(name);
-  }
+  };
 
   useEffect(() => {
-    const wordElement = document.querySelector(".word");
-    const letterElement = document.querySelector(".letter");
-    if (wordElement && !wordElement.classList.contains("current")) {
-      addClass(wordElement, "current");
+    const wordElement = document.querySelector('.word');
+    const letterElement = document.querySelector('.letter');
+    if (wordElement && !wordElement.classList.contains('current')) {
+      addClass(wordElement, 'current');
     }
-  
-    if (letterElement && !letterElement.classList.contains("current")) {
-      addClass(letterElement, "current");
+
+    if (letterElement && !letterElement.classList.contains('current')) {
+      addClass(letterElement, 'current');
     }
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    const handleKeyUpListener = (event:any) => handleKeyUp(event);
+    document.addEventListener('keyup', handleKeyUpListener);
+    return () => {
+      document.removeEventListener('keyup', handleKeyUpListener);
+    };
+  }, [gameOver]);
 
   return (
     <>
@@ -40,7 +115,7 @@ function App() {
               </button>
               <button
                 onClick={() => setTime(30)}
-                className={`py-2 px-4 rounded-lg text-[#62665e] font-semibold ${time === 30 ? 'text-[#e2b71e]' : ' hover:text-white'}`}
+                className={`py-2 px-4 rounded-lg text-[#62665e] font-semibold ${time === 30 ? 'text-[#e2b71e]' : 'hover:text-white'}`}
               >
                 30
               </button>
@@ -55,9 +130,11 @@ function App() {
             className="flex flex-wrap gap-4 text-4xl justify-center max-w-7xl p-4 leading-10 font-normal tracking-wide relative"
           >
             {words.map((word, wordIndex) => (
-              <div key={wordIndex} className='word'>
+              <div key={wordIndex} className="word">
                 {word.split('').map((letter, letterIndex) => (
-                  <span key={letterIndex} className='letter'>{letter}</span>
+                  <span key={letterIndex} className="letter">
+                    {letter}
+                  </span>
                 ))}
               </div>
             ))}
